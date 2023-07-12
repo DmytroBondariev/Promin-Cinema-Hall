22f517612fbe07693132f318e5bdf5892c5b27d1
Dmytry Bondariev <bondarievmd@gmail.com>
2023-07-12 13:39:15 +0300

    Fixed README.md; removed comments from settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -95,12 +95,6 @@ DATABASES = {
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
